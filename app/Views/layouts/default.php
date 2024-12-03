d52f64c7c0a86c2e30f7293dd79140cec25d3dcb
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-12-03 11:46:35 +0530

    added tailwindcss and flowbite
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

---
+++ app/Views/layouts/default.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/style.css ">
+    
+</head>
+<body>
+    <?= $this->renderSection("body"); ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+</body>
+</html>
